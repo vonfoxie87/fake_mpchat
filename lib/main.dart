9408eaa98fa53fa6103721b199bf7ad3a0ec69a6
vonfoxie87 <nils@nh-vos.nl>
2025-03-15 21:48:32 +0100

    GPS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 void main() {
   runApp(ChatApp());
@@ -21,68 +23,127 @@ class ChatApp extends StatelessWidget {
   }
 }
 
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
 
-class ChatScreen extends StatelessWidget {
-  final List<Map<String, dynamic>> messages = [
-    {
-      "text": "Hallo Anna,\nIk ben heel geintresseerd in deze fiets en zou er graag €75 voor bieden. Mijn zoon is bijna 3 jaar en ongeveer een meter. Denk je dat hij nog even vooruit kan met deze fiets? Ik werk zelf in Delft dus dat is lekker in de buurt.\n\nGroetjes, \n\nStefan",
-      "isMe": true,
-      "time": "13:22",
-      "status": "gelezen"
-    },
-    {
-      "text": "Ha Stefan,\n\nMijn zoon heeft deze week een nieuwe fiets gekregen, hij is ruim 4.5 jaar en heeft veel plezier van deze fiets gehad.\n\nHij zou nog langer met de fiets kunnen doen maar kreeg een fiets doorgeschoven uit de familie. Het stuur en zadel kunnen allebei omhoog gezet worden, dus zou zeker geen probleem moeten zijn.\n\n Anna",
-      "isMe": false,
-      "time": "13:31"
-    },
-    {
-      "text": "Oh super dat klinkt goed! Zou ik de fiets van je kunnen overnemen?",
-      "isMe": true,
-      "time": "13:34",
-      "status": "gelezen"
-    },
-    {
-      "text": "Ik zou eventueel vandaag na mn werk door kunnen rijden, werk tot 16:15u. Ik hoor het wel.",
-      "isMe": false,
-      "time": "13:35"
-    },
-    {
-      "text": "Vandaag lukt niet, morgen kan wel. Is dat een optie?",
-      "isMe": false,
-      "time": "14:20"
-    },
-    {
-      "text": "Helemaal goed.",
-      "isMe": true,
-      "time": "14:26",
-      "status": "gelezen"
-    },
-    {
-      "text": "Wat is je adres? Wil je het geld contant of kan het ook via een tikkie?",
-      "isMe": true,
-      "time": "14:27",
-      "status": "gelezen"
-    },
-    {
-      "text": "Liefst digitaal met een qr code",
-      "isMe": false,
-      "time": "14:29"
-    },
-    {
-      "text": "Adres is **********. Dit is mijn nummer: 0652794628, bel maar of app maar als je in de buurt ben, Dan weet ik ongeveer hoelaat.",
-      "isMe": false,
-      "time": "14:30"
-    },
-    {
-      "text": "Prima, tot morgen",
-      "isMe": true,
-      "time": "14:32",
-      "status": "gelezen"
-    },
-  ];
+class _ChatScreenState extends State<ChatScreen> {
+  String _street = 'Locatie ophalen...'; // Start met een bericht
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {_street = "Locatie niet beschikbaar";});
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          _street = "Toestemming geweigerd";
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _street = "Locatie permanent geweigerd";
+      });
+      return;
+    }
+
+    // Verkrijg de huidige locatie
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.best,
+    );
+
+    // Converteer de coördinaten naar een adres
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    // Stel de straatnaam in als deze beschikbaar is
+    if (placemarks.isNotEmpty) {
+      setState(() {
+        _street = placemarks.first.street ?? "Onbekende straat";
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> messages = [
+      {
+        "text":
+            "Hallo Anna,\nIk ben heel geintresseerd in deze fiets en zou er graag €75 voor bieden. Mijn zoon is bijna 3 jaar en ongeveer een meter. Denk je dat hij nog even vooruit kan met deze fiets? Ik werk zelf in Delft dus dat is lekker in de buurt.\n\nGroetjes, \n\nStefan",
+        "isMe": true,
+        "time": "13:22",
+        "status": "gelezen"
+      },
+      {
+        "text":
+            "Ha Stefan,\n\nMijn zoon heeft deze week een nieuwe fiets gekregen, hij is ruim 4.5 jaar en heeft veel plezier van deze fiets gehad.\n\nHij zou nog langer met de fiets kunnen doen maar kreeg een fiets doorgeschoven uit de familie. Het stuur en zadel kunnen allebei omhoog gezet worden, dus zou zeker geen probleem moeten zijn.\n\n Anna",
+        "isMe": false,
+        "time": "13:31"
+      },
+      {
+        "text": "Oh super dat klinkt goed! Zou ik de fiets van je kunnen overnemen?",
+        "isMe": true,
+        "time": "13:34",
+        "status": "gelezen"
+      },
+      {
+        "text": "Ik zou eventueel vandaag na mn werk door kunnen rijden, werk tot 16:15u. Ik hoor het wel.",
+        "isMe": false,
+        "time": "13:35"
+      },
+      {
+        "text": "Vandaag lukt niet, morgen kan wel. Is dat een optie?",
+        "isMe": false,
+        "time": "14:20"
+      },
+      {
+        "text": "Helemaal goed.",
+        "isMe": true,
+        "time": "14:26",
+        "status": "gelezen"
+      },
+      {
+        "text": "Wat is je adres? Wil je het geld contant of kan het ook via een tikkie?",
+        "isMe": true,
+        "time": "14:27",
+        "status": "gelezen"
+      },
+      {
+        "text": "Liefst digitaal met een qr code",
+        "isMe": false,
+        "time": "14:29"
+      },
+      {
+        "text": "Adres is $_street. Dit is mijn nummer: 0652794628, bel maar of app maar als je in de buurt ben, Dan weet ik ongeveer hoelaat.",
+        "isMe": false,
+        "time": "14:30"
+      },
+      {
+        "text": "Prima, tot morgen",
+        "isMe": true,
+        "time": "14:32",
+        "status": "gelezen"
+      },
+    ];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFFEDA566),
@@ -101,11 +162,11 @@ class ChatScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "Alpina Yabber 12 inch", 
+                  "Alpina Yabber 12 inch",
                   style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
                 ),
                 Text(
-                  "Anna", 
+                  "Anna",
                   style: TextStyle(fontSize: 14, color: Colors.white),
                 ),
               ],
@@ -128,7 +189,6 @@ class ChatScreen extends StatelessWidget {
               padding: EdgeInsets.all(10),
               itemCount: messages.length,
               itemBuilder: (context, index) {
-                //bool isLastMessage = index == messages.length - 1;
                 bool isMe = messages[index]['isMe'] ?? false;
                 String status = messages[index]['status'] ?? "verzonden";
                 Color checkColor = status == "gelezen" ? Colors.green : Colors.grey;
@@ -167,7 +227,7 @@ class ChatScreen extends StatelessWidget {
                               messages[index]['time'],
                               style: TextStyle(fontSize: 12, color: Colors.grey),
                             ),
-                            if (isMe) ...[
+                            if (isMe) ...[ 
                               SizedBox(width: 5),
                               Icon(Icons.done_all, color: checkColor, size: 16),
                             ],
@@ -176,7 +236,6 @@ class ChatScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-
                 );
               },
             ),
@@ -199,7 +258,7 @@ class ChatScreen extends StatelessWidget {
                     Container(
                       padding: EdgeInsets.all(2),
                       decoration: BoxDecoration(
-                        color: const Color(0xFF0E4F85), 
+                        color: const Color(0xFF0E4F85),
                         borderRadius: BorderRadius.circular(12)
                       ),
                       child: IconButton(
@@ -226,7 +285,7 @@ class ChatScreen extends StatelessWidget {
                       ],
                     ),
                     OutlinedButton.icon(
-                      onPressed: () {}, 
+                      onPressed: () {},
                       icon: Icon(Icons.handshake_outlined, color: const Color(0xFF0E4F85)),
                       label: Text("Doe een voorstel", style: TextStyle(color: const Color(0xFF0E4F85))),
                       style: OutlinedButton.styleFrom(
